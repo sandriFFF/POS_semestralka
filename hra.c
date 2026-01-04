db8168d4bc5d192b590b5d8df2b1c494da07ecc1
sandriFFF <sandra.frnkova@gmail.com>
2026-01-04 17:07:44 +0100

    initial design

---
+++ hra.c
@@ -0,0 +1,5 @@
+//
+// Created by Alex on 1/4/2026.
+//
+
+#include "hra.h"
---
+++ hra.h
@@ -0,0 +1,8 @@
+//
+// Created by Alex on 1/4/2026.
+//
+
+#ifndef SEMESTRALKA_HRA_H
+#define SEMESTRALKA_HRA_H
+
+#endif //SEMESTRALKA_HRA_H
